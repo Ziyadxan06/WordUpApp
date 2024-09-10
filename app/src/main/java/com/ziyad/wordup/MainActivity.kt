3b683bb46623160375f462e7797f330b8fafeaad
Ziyadxan Dovletov <ziyadxan.dovletov06@gmail.com>
2024-09-10 13:52:30 +0400

    Implement bottom navigation graph setup in MainActivity

--- app/src/main/java/com/ziyad/wordup/MainActivity.kt
+++ app/src/main/java/com/ziyad/wordup/MainActivity.kt
@@ -5,11 +5,20 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.ziyad.wordup.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var bottomNavigationView: BottomNavigationView
+    private lateinit var navController: NavController
+    private lateinit var navHostFragment: NavHostFragment
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,5 +30,19 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
+
+        bottomNavigationView = binding.bottomNavigation
+
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.learningWordsFragment,
+                R.id.masteredWordsFragment
+            )
+        )
+
+        bottomNavigationView.setupWithNavController(navController)
     }
 }
